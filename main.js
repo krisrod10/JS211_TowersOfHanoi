72b675513fb569ec2ec97e058b6106893aa42917
krisrod10 <82917336+krisrod10@users.noreply.github.com>
2021-07-16 20:32:11 -0500

    update

--- main.js
+++ main.js
@@ -17,12 +17,13 @@ const rl = readline.createInterface({
         // * 1 is the smallest
 
 let stacks = {
- name: 'stack1', a : [4,3,2,1],           //0
- name: 'stack2', b : [1,4,3,2],           //1
- name: 'stack3', c : [2,3,1,4]            //2
+  a : [4,3,2,1],    //0
+  b : [],           //1
+  c : []            //2
 };
 
 // Start here. What is this function doing?
+// logging a visual representation
 const printStacks = () => {
   console.log("a: " + stacks.a);
   console.log("b: " + stacks.b);
@@ -30,52 +31,85 @@ const printStacks = () => {
 }
 
 // Next, what do you think this function should do?
-const movePiece = () => {
+// taking in a string and storing it inside startStack same process with endStack
+// starStack starts to become the key for stacks
+const movePiece = (startStack, endStack) => {
+  //code here
+  startStack = stacks[startStack]
+  //cosnsole.log(start + startStack)
+  endStack = stacks[endStack]
 
+  let fromMove = startStack.pop()
+  endStack.psuh(fromMove);
+}
+const letter1 = (startStack, endStack) => {
+  if((startStack === 'a' || startStack === 'b' || startStack === 'c') && (endStack === 'a' || endStack === 'b' || endStack === 'c')){
+    return true
+  }else  return false
 
 }
-
 // Before you move, should you check if the move it actually allowed? Should 3 be able to be stacked on 2
-const isLegal = () => {
-    const notLegal = (s,e) => {
-      let start = stacks[s]
-      let end = stacks[e]
-  
-      console.log('endstack-1;', end.length-1)
-      console.log('stacks c: ', stacks.c)
-      console.log('start.length: ', start.length)
-  
-  
-      if((start[start.length-1] < end[end.length-1]) || (end.length === 0)){
-  return true
-      }else if((start.length === 0) && (end.length === 0)){
-        return false
-      }
-      else{
-        return false
-      }
-  
+const isLegal = (startStack, endStack) => {
+ // Your code here   
+  const goodLetter = letter1(startStack, endStack)
+  if(goodLetter){
+    console.log('wrong')
+    return false
   }
 }
 
-// What is a win in Towers of Hanoi? When should this function run?
-const checkForWin = () => {
-  let checkForWin = 'You win'
-  if   (stacks === 'a' [1,2,3,4] ){
-    return checkForWin
-  }else if   (stacks === 'b' [1,2,3,4]){
-    return checkForWin
-  }else if  (stacks === 'c'[1,2,3,4])
-    return checkForWin
-  }
+startStack = stacks[startStack];
+
+let startR = startStack.length
+
+let indexR = startR -1
+
+let valueStartStack = startStack[indexR]
+console.log(typeof(valueSTartSTack));
+
+endSTack = stacks[endStacks]
+//console.log(endStack.length);
+
+let endK = endStack.length
+let indexEndK = endK -1
+
+let valueEndStack = endStack[indexEndK]
+console.log(valueEndStack);
+
+if(valueEndStack < valueEndStack || valueEndStack === unidefined){
+  return true
+}else if (valueStartStack > valueEndStack){
+  console.log('not a legal move')
+  return false
+}
+
+
+
+
+// What is a win in Towers of Hani? When should this function run?
+const checkForWin = (startStack, endStack) => {
+// Your code here 
+if((startStack.b[0] === 4 && startStack.b[1] === 3 && startStack.b[2] === 2 && startStack.b[3] === 1) ||
+(endStack.c[0] === 4 && endStack.c[1] === 3 && endStack.b[2] === 2 && endStack.v[3] === 1)){
+  console.log('You win')
+  return true
+}else {
+  return false
+}
+
+
+}
 
 
 
 // When is this function called? What should it do with its argument?
 const towersOfHanoi = (startStack, endStack) => {
-  for (let s=0; s<stacks.length; s++){
-let startStack = stacks[e];
-  }
+ // Your code here 
+ console.log(isLegal(startStack, endStack))
+ if(isLegal(startStack, endStack)){
+   movePiece(startStack, endStack)
+ }
+
 }
 
 const getPrompt = () => {
